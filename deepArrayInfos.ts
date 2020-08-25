fac8cbe05a08498f8eed1df87460e2bf6bfaceab
QuentinGuichaoua <39485623+quentg77@users.noreply.github.com>
2020-08-25 11:52:27 +0200

    ex 1 OK

--- deepArrayInfos.ts
+++ deepArrayInfos.ts
@@ -14,7 +14,10 @@ export const deepArraySum = (input: deepArray): number => {
 	}
 };
 
-export const deepArrayDepth = (input: deepArray, actualDepth: number = 1): number => {
+export const deepArrayDepth = (
+	input: deepArray,
+	actualDepth: number = 1
+): number => {
 	if (typeof input === "number") {
 		return 0;
 	} else {
@@ -27,4 +30,38 @@ export const deepArrayDepth = (input: deepArray, actualDepth: number = 1): numbe
 
 		return depth;
 	}
-}
+};
+
+export const deepArrayMax = (
+	input: deepArray,
+	currentMaxValue: number = Number.MIN_SAFE_INTEGER
+): number => {
+	if (typeof input === "number") {
+		return currentMaxValue >= input ? currentMaxValue : input;
+	} else {
+		let max = currentMaxValue;
+
+		input.map((item) => {
+			max = deepArrayMax(item, max);
+		});
+
+		return max;
+	}
+};
+
+export const deepArrayMin = (
+	input: deepArray,
+	currentMinValue: number = Number.MAX_SAFE_INTEGER
+): number => {
+	if (typeof input === "number") {
+		return currentMinValue <= input ? currentMinValue : input;
+	} else {
+		let min = currentMinValue;
+
+		input.map((item) => {
+			min = deepArrayMin(item, min);
+		});
+
+		return min;
+	}
+};
--- testDeepArrayInfo.ts
+++ testDeepArrayInfo.ts
@@ -1,6 +1,8 @@
-import { deepArray, deepArrayDepth, deepArraySum } from "./deepArrayInfos";
+import { deepArray, deepArrayDepth, deepArrayMax, deepArrayMin, deepArraySum } from "./deepArrayInfos";
 
 const list: deepArray = [1, [[2], 3], [4], 5, [6, 42, [[86], [[12]], 1337]], 1];
 
-console.log(deepArraySum(list));
-console.log(deepArrayDepth(list));
+console.log("sum", deepArraySum(list));
+console.log("depth", deepArrayDepth(list));
+console.log("max", deepArrayMax(list));
+console.log("min", deepArrayMin(list));
